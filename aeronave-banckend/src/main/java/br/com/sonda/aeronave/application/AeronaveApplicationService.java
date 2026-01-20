627b3c6147dd0d76c50710f40ab2c375c7e13b6b
Lucas Rocha <lucas.rocha-dev@outlook.com>
2026-01-20 17:38:08 -0300

    Adequação de projeto para arquitetura limpa 1

---
+++ aeronave-banckend/src/main/java/br/com/sonda/aeronave/application/AeronaveApplicationService.java
@@ -0,0 +1,12 @@
+package br.com.sonda.aeronave.application;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AeronaveApplicationService
+{
+
+}
